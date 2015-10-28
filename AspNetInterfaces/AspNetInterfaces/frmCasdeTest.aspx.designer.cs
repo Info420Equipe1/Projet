241c176019327b5735676e3a90f0a3c2756d412b
Metever <marcelleblond@live.ca>
2015-10-28 11:06:23 -0400

    Ajout Projet ASP

---
+++ AspNetInterfaces/AspNetInterfaces/frmCasdeTest.aspx.designer.cs
@@ -0,0 +1,177 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace AspNetInterfaces {
+    
+    
+    public partial class frmCasdeTest {
+        
+        /// <summary>
+        /// Contrôle form1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Contrôle ToolkitScriptManager1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ToolkitScriptManager1;
+        
+        /// <summary>
+        /// Contrôle txtCode.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCode;
+        
+        /// <summary>
+        /// Contrôle txtNom.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNom;
+        
+        /// <summary>
+        /// Contrôle txtDifficulté.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList txtDifficulté;
+        
+        /// <summary>
+        /// Contrôle txtPriorité.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList txtPriorité;
+        
+        /// <summary>
+        /// Contrôle txtDateCreation.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDateCreation;
+        
+        /// <summary>
+        /// Contrôle txtDateCreation_CalendarExtender.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::AjaxControlToolkit.CalendarExtender txtDateCreation_CalendarExtender;
+        
+        /// <summary>
+        /// Contrôle txtDateLivraison.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDateLivraison;
+        
+        /// <summary>
+        /// Contrôle txtDateLivraison_CalendarExtender.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::AjaxControlToolkit.CalendarExtender txtDateLivraison_CalendarExtender;
+        
+        /// <summary>
+        /// Contrôle txtTypeTest.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTypeTest;
+        
+        /// <summary>
+        /// Contrôle rtxtObjectif.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox rtxtObjectif;
+        
+        /// <summary>
+        /// Contrôle rtxtDescription.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox rtxtDescription;
+        
+        /// <summary>
+        /// Contrôle rtxtDivers.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox rtxtDivers;
+        
+        /// <summary>
+        /// Contrôle DataGridBilletTravail.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataGrid DataGridBilletTravail;
+        
+        /// <summary>
+        /// Contrôle btnCopier.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCopier;
+        
+        /// <summary>
+        /// Contrôle btnEnregistrer.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEnregistrer;
+        
+        /// <summary>
+        /// Contrôle btnAnnuler.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAnnuler;
+    }
+}
---
+++ AspNetInterfaces/AspNetInterfaces/frmCreerProjet.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmCasTest.aspx.cs" Inherits="AspNetInterfaces.bonjours" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" runat="server" type="text/css" href="~/StyleCreerProjet.css" />
+    <title><asp:Literal runat="server" Text="<%$ Resources:Resource1, titre_bonjours %>" /></title>
+    </head>
+<body>
+    <form id="form1" runat="server">
+        <div id="CasTestInfo">
+            <div id="lblColumn1">
+                <div class="info">
+                    <asp:Label runat="server" Text="Code: " class="lblColum1"/>
+                    <asp:TextBox runat="server" ID="txtCodeCasTest" class="txtColum1"/>
+                </div>
+                <div class="info">
+                    <asp:Label runat="server" Text="Nom: " CssClass="lblColum1"/>
+                    <asp:TextBox runat="server" ID="txtNomCasTest" CssClass="txtColum1"/>
+                </div>
+                <div class="info">
+                    <asp:Label runat="server" Text="Chef de projet: " CssClass="lblColum1"/>
+                    <asp:DropDownList runat="server" ID="txtChefProjetCasTest" CssClass="txtColum1"/>
+                </div>
+                <div class="info">
+                    <asp:Label runat="server" Text="Date Création: " CssClass="lblColum1"/>
+                    <asp:TextBox runat="server" ID="txtDateCreationCasTest" CssClass="txtColum1"/>
+                </div>
+                <div class="info">
+                    <asp:Label runat="server" Text="Date Livraison: " CssClass="lblColum1"/>
+                    <asp:TextBox runat="server" ID="txtDateLivraisonCasTest" CssClass="txtColum1"/>
+                </div>
+                <div class="info">
+                    <asp:Label runat="server" Text="Version du jeu: " CssClass="lblColum1"/>
+                    <asp:TextBox runat="server" ID="txtVersionJeuCasTest" CssClass="txtColum1"/>
+                </div>        
+            </div>
+            <div id="CasTestObj">
+                <asp:Label runat="server" Text="Objectif: " CssClass="lblColum2"/>
+                <asp:TextBox runat="server" ID="rtxtObjectifCasTest" TextMode="MultiLine" CssClass="txtColum2" Height="180px" Width="730px"/>
+            </div>
+            <div id="CasTestDesc">
+                <asp:Label runat="server" Text="Description: " CssClass="lblColum2"/>
+                <asp:TextBox runat="server" ID="rtxtDescriptionCasTest" TextMode="MultiLine" CssClass="txtColum2" Height="180px" Width="730px"/>
+            </div>
+            <div id="CasTestDiv">
+                <asp:Label runat="server" Text="Divers: " CssClass="lblColum2"/>
+                <asp:TextBox runat="server" ID="rtxtDiversCasTest" TextMode="MultiLine" CssClass="txtColum2" Height="180px" Width="730px"/>
+            </div>
+            <asp:DataGrid id="DataGrid" runat="server"></asp:DataGrid>
+            
+        </div>
+        <div id="btnCopierEnregistrerAnnuler">
+            <div id="btnCentre">
+                <asp:Button runat="server" ID="btnCopier" Text="Copier" />
+            </div>
+            <div id="btnDroit">
+                <asp:Button runat="server" ID="btnEnregistrer" Text="Enregistrer" />
+                <asp:Button runat="server" ID="btnAnnuler" Text="Annuler" />
+            </div>
+        </div>    
+    </form>
+</body>
+</html>
