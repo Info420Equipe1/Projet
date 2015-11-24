02d450ddcd1a172a9031f1fdc2d9f1200ec3d741
lam18 <lam18@hotmail.ca>
2015-11-24 09:37:28 -0500

    Ajout id test

--- TexcelWeb/TexcelWeb/Interfaces/recherche.aspx.designer.cs
+++ TexcelWeb/TexcelWeb/Interfaces/recherche.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace TexcelWeb {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl txtDerniereConnexion;
         
         /// <summary>
+        /// Contrôle menuRecherche.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menuRecherche;
+        
+        /// <summary>
         /// Contrôle FrmCasTest.
         /// </summary>
         /// <remarks>
