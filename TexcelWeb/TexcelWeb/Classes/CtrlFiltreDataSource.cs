188a49cbbeb6c9603163b27f6643da511adb76c1
Metever <marcelleblond@live.ca>
2015-12-19 15:15:15 -0500

    Revue de code - CtrlFiltreDataSource

--- TexcelWeb/TexcelWeb/Classes/CtrlFiltreDataSource.cs
+++ TexcelWeb/TexcelWeb/Classes/CtrlFiltreDataSource.cs
@@ -5,8 +5,18 @@ using System.Web;
 
 namespace TexcelWeb.Classes
 {
+    //
+    //
+    // Control Filtre Datasource
+    // Cette classe contient tous les méthodes et traitements en lien la liaison des filtres de
+    // recherche avec les entityDataSources
+    //
+    //
+    
     public class CtrlFiltreDataSource : CtrlController
     {
+        
+        // Récupère tous les nom des filtres dans la table FiltreDataSource
         public static List<string> GetAllNomFiltre()
         {
             List<string> nomFiltres = new List<string>();
@@ -17,6 +27,7 @@ namespace TexcelWeb.Classes
             return nomFiltres;
         }
 
+        // Récupère le nom du dataSource en fonction du nom du filtre passeé en paramètre
         public static string GetDataSourceName(string _filterName)
         {
             return context.FiltreDataSource.Where(x => x.nomFiltre == _filterName).First().nomDataSource;
